65c003a1822dc135b2e79c78fe00b7b2e7a8efe0
Erick Lochner <ericklochner@gmail.com>
2023-02-28 19:33:35 +0200

    Completed lesson 1

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  data() {
+    return {
+      product: "Socks",
+      notProduct: "Not socks",
+      description: "This is shoes without socks"
+    }
+  },
+})
